57d9ccbbb6cb69721e6507285701538be6a0bdd9
jfcontart <jfcontart@idemobi.com>
2020-12-12 12:50:36 +0100

    [INIT]

---
+++ TestDotNetDeploiment/AspEmpty/AspEmpty/Startup.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Model;
+using System.Web;
+using System.IO;
+using System.Text;
+
+namespace AspEmpty
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public static Dictionary<string, string> ParseQueryString(HttpRequest sRequest)
+        {
+            var nvc = HttpUtility.ParseQueryString(sRequest.QueryString.Value);
+            return nvc.AllKeys.ToDictionary(k => k, k => nvc[k]);
+        }
+
+        public static string RequestBody(HttpRequest sRequest)
+        {
+            string documentContents = "error";
+            //sRequest.Form["body"];
+            //string requestContent = sRequest.co.ReadAsStringAsync().Result;
+            //documentContents = await sRequest.BodyReader.ReadAsync().Result;
+            return documentContents;
+        }
+
+        public static Dictionary<string, string> ParseHeaders(HttpRequest sRequest)
+        {
+            Dictionary<string, string> rReturn = sRequest.Headers.ToDictionary(a => a.Key, a => string.Join(";", a.Value));
+            return rReturn;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDelete("/", context => {
+                    return Task.CompletedTask;
+                });
+                endpoints.MapGet("/", async context =>
+                {
+                    Request tRequest = new Request();
+                    tRequest.RequestParameters = ParseQueryString(context.Request);
+                    tRequest.Headers = ParseHeaders(context.Request);
+                    tRequest.Body = "no body in method get";
+                    test tTest = new test();
+                    await context.Response.WriteAsync(tTest.HelloWithRequest(tRequest));
+                });
+                //endpoints.MapPost("/", async context =>
+                //{
+                //    Request tRequest = new Request();
+                //    tRequest.Method = context.Request.Method;
+                //    tRequest.RequestParameters = ParseQueryString(context.Request);
+                //    tRequest.Headers = ParseHeaders(context.Request);
+
+                //    using (var reader = new StreamReader(context.Request.Body))
+                //    {
+                //        var tBodyString = reader.ReadToEnd();
+                //        tRequest.Body = tBodyString;
+                //    }
+
+                //    //tRequest.Body = context.Request.BodyReader.ToString();
+                //    test tTest = new test();
+                //    await context.Response.WriteAsync(tTest.HelloWithRequest(tRequest));
+                //});
+                endpoints.MapPost("/", context => {
+                    Request tRequest = new Request();
+                    tRequest.Method = context.Request.Method;
+                    tRequest.RequestParameters = ParseQueryString(context.Request);
+                    tRequest.Headers = ParseHeaders(context.Request);
+
+                    var body = new StreamReader(context.Request.BodyReader.AsStream());
+                    //body.BaseStream.Seek(0, SeekOrigin.Begin);
+                    var requestBody = body.ReadToEnd();
+                    tRequest.Body = requestBody;
+                    test tTest = new test();
+                    context.Response.WriteAsync(tTest.HelloWithRequest(tRequest));
+                    return Task.CompletedTask;
+                });
+            });
+        }
+    }
+}
---
+++ TestDotNetDeploiment/Assets/Model/test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class Request
+    {
+        public string Method;
+        public Dictionary<string, string> Headers = new Dictionary<string, string>();
+        public Dictionary<string, string> RequestParameters = new Dictionary<string, string>();
+        public string Body;
+    }
+
+    public class test
+    {
+        public static string HelloStatic()
+        {
+            return "Yessss I won! by static method";
+        }
+        public string Hello()
+        {
+            return "Yessss I won! by method";
+        }
+
+        public string HelloWithRequest(Request sRequest, string sMoreInfos = "")
+        {
+            string tParam = string.Empty;
+            string tHeaders = string.Empty;
+            foreach (KeyValuePair<string, string> keyValuePair in sRequest.RequestParameters)
+            {
+                tParam += "<li>" + keyValuePair.Key + " : " + keyValuePair.Value + "</li>";
+            }
+            foreach (KeyValuePair<string, string> keyValuePair in sRequest.Headers)
+            {
+                tHeaders += "<li>" + keyValuePair.Key + " : " + keyValuePair.Value + "</li>";
+            }
+            return "<html><body><h1>Yessss I won!</h1> " +
+                "<h2>Method </h2><p>" + sRequest.Method + "</p>" +
+                "<h2>Request Param </h2><ul>" + tParam + "</ul>" +
+                "<h2>Headers Param </h2><ul>" + tHeaders + "</ul>" +
+                "<h2>More infos </h2><p>" + sMoreInfos + "</p>" +
+                "<h2>Body </h2><p>" + sRequest.Body + "</p>" +
+                "</body></html>";
+        }
+    }
+}
